bd918359ae72e4271daa046dc21dd266d3534eca
nauvalachid <nauvalachid@gmail.com>
2023-05-18 18:33:53 +0700

    case 2

--- 117_singlelinkedlist/117_singlelinkedlist.cpp
+++ 117_singlelinkedlist/117_singlelinkedlist.cpp
@@ -151,3 +151,23 @@ int main() {
 				system("pause");
 				system("cls");
 				break;
+			case 2:
+				if (listEmpty())
+				{
+					cout << "List kosong" << endl;
+					system("pause");
+					system("cls");
+					break;
+				}
+
+				int nim;
+				cout << "Masukkan NIM: ";
+				cin >> nim;
+				if (deleteNode(nim)) {
+					cout << "nim: " << nim << "berhasil dihapus" << endl;
+					system("pause");
+					system("cls");
+				}
+				else
+					cout << "Data tidak ditemukan" << endl;
+				break;
