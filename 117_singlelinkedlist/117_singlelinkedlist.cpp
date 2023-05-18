9e2434eb770c8b4f6a8d3940208553b42ca63ea7
nauvalachid <nauvalachid@gmail.com>
2023-05-18 17:39:48 +0700

    menampilkan void addNode

--- 117_singlelinkedlist/117_singlelinkedlist.cpp
+++ 117_singlelinkedlist/117_singlelinkedlist.cpp
@@ -6,3 +6,46 @@ struct Node {
 	string name;
 	Node* next;
 };
+
+Node* START = NULL;
+
+void addNode() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukkan NIM: ";
+	cin >> nim;
+	cout << "Masukkan Nama: ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+
+	if (START == NULL || nim <= START->noMhs{
+		if (START != NULL && nim == START->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+		}
+
+	Node* previous = START;
+	Node* current = START;
+
+	while ((curent != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = current;
+		current = current->next;
+	}
+
+	nodeBaru->next = current;
+	previous->next = nodeBaru;
+}
